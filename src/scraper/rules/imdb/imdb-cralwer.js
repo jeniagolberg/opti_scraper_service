aa1228dfd1f1fa4b6361bceaf39683ac84b51fa6
Jenia Golberg <jeniagolberg@Jenias-MacBook-Air.local>
2020-11-22 20:51:40 +0200

    implemented crawler logics now throws error when atemmting to save

--- src/scraper/rules/imdb/imdb-cralwer.js
+++ src/scraper/rules/imdb/imdb-cralwer.js
@@ -1,22 +1,82 @@
 const dbService = require('./../../../db')
-module.exports = { 
+
+const logError = console.error;
+
+const serializeBudget = rawText => rawText.split(' ')[0].split(':')[1];
+
+const serializeCwg = rawText => rawText.split(':')[1];
+
+const serializeRuntime = rawText => rawText.split(':')[1]
+
+const serializeReleaseDate = rawText => {
+    let day = rawText.match(/ \d{1,2} /)[0].replace(/ /g, '')
+    let month = rawText.match(/\s(?:January| February |March|April|May|June|July|August|September|October|November|December)\b/)[0].replace(/ /g, '');
+    let year = rawText.match(/\s*\d{4}/)[0].replace(/ /g, '')
+
+    return {day,month,year};
+}
+
+const getContext = (article,context) => article.split('\n').filter(ele => ele.startsWith(context))[0]
+
+const serializeGenres = rawText => rawText.split(":")[1]
+                                            .replace(/[ `~!@#$%^&*()_|+\=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+
+// extracts data for : budget, realese date, Runtime, Cumulative Worldwide Gross / Generes
+const getArticleData = async (element, xpath) => {
+    try {
+        let articleElement = (await element.$x(xpath))[0];
+        let text =  await element.evaluate(el => el.innerText, articleElement)
+        return text;
+    } catch (err) {
+        logError(`Error on text retrival by article,\n${xpath}\n${err}`);
+    }
+}   
+
+//extarcts data for : full movie name and rating 
+const getDataByAncestors = async (element, ancestors) => {
+    try{
+        return await page.$eval(ancestors, el => el.innerText);
+    } catch (err) {
+        logError(`Error on text retrival by ancestors,\n${ancestors}, \n${err}`)
+    }
+}
+
+const extractData = async (page) => {
     
+    let detailsText = await getArticleData(page, '//div[@id="titleDetails"]');
+    serializeBudget
+    //details article
+    let budget = serializeBudget(getContext(detailsText, "Budget:"));
+    let cwg = serializeCwg(getContext(detailsText, "Cumulative Worldwide Gross:"));
+    let releaseDate = serializeReleaseDate(getContext(detailsText, "Release Date:"));
+    let runtime = serializeRuntime(getContext(detailsText, "Runtime:"));
+
+    //title data
+    let fullName = await getDataByAncestors(page,[".title_bar_wrapper .title_wrapper h1"]);
+    let rating = await getDataByAncestors(page, [".title_bar_wrapper .ratingValue"]);
+
+
+    //genres
+    let storyLineText = await getArticleData(page, '//div[@id="titleStoryLine"]');
+    let genres = await serializeGenres(getContext(storyLineText, "Genres:"));
+
+    return {budget,cwg,releaseDate,runtime,fullName,rating,genres}
+}
+
+
+module.exports = { 
     /**
      * extracts required data from a single page
      */
-    extractImdbData: function(driver) {
-        /*
-        todo: using driver.findElements to get the relevent data
-        use save callback to write to db
-        */
+    extractImdbData: async function(page) {
+        console.log(await extractData(page));
     },
-
     /**
      *  returns the next link to extract data from(when its from the page)
      *  if false then no crawling is neseccery.
      *  currently returns false since for now we crawl a list of given links.
     */
-    goToNextPage: function(depth,driver, setNext) { 
+    goToNextPage: function(depth, driver, setNext) { 
         return false
     },
     /**
--- src/scraper/scrape.js
+++ src/scraper/scrape.js
@@ -1,34 +1,66 @@
 
 const {extract, crawl, save} = require('./rules/index');
-const {Builder} = require('selenium-webdriver');
+const puppeteer = require('puppeteer');
 
+const start_urls = 
+`https://www.imdb.com/title/tt0451279,
+https://www.imdb.com/title/tt0114369,
+https://www.imdb.com/title/tt0120815,
+https://www.imdb.com/title/tt0103064,
+http://www.imdb.com/title/tt0078748,
+https://www.imdb.com/title/tt0032553,
+https://www.imdb.com/title/tt7286456,
+https://www.imdb.com/title/tt4633694,
+https://www.imdb.com/title/tt0114709,
+https://www.imdb.com/title/tt0119217,
+https://www.imdb.com/title/tt0211915,
+https://www.imdb.com/title/tt0056592,
+https://www.imdb.com/title/tt0095016,
+http://www.imdb.com/title/tt0468569,
+https://www.imdb.com/title/tt0050083,
+https://www.imdb.com/title/tt0110912,
+https://www.imdb.com/title/tt0080684,
+https://www.imdb.com/title/tt0133093,
+https://www.imdb.com/title/tt0111161/,
+https://www.imdb.com/title/tt0268978,
+https://www.imdb.com/title/tt3315342,
+https://www.imdb.com/title/tt0117731/,
+https://www.imdb.com/title/tt2096673/,
+imdb.com/title/tt0092099,
+http://www.imdb.com/title/tt0317219,
+http://www.imdb.com/title/tt1306980,
+http://www.imdb.com/title/tt0110357,
+https://www.imdb.com/title/tt0080179,
+https://www.imdb.com/title/tt0093773,
+https://www.imdb.com/title/tt0087928`
 
 var scrapeController = async function(req,res,next){
 
-    if(!req.headers.start_urls.length){
-        req.startUrls = [req.startUrls]
-    }
+    // if(!req.headers.start_urls.length){
+    //     req.startUrls = [req.startUrls]
+    // }
 
-    const driver = await new Builder().forBrowser('chrome').build();
-
-    run(driver,req.headers.start_urls, req.headers.crawl_depth);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    run(page,start_urls, 1);
     res.status(200);
     res.send('crawling started!')
 }
 
-var run = async function(driver,links, requiredDepth){
+var run = async function(page,links, requiredDepth){
 
-    for(const link of links.split(',')){
+    for(const link of links.split(',\n')){
         console.log(link)
         let currentLink = link;
         do {
             try {
-                await driver.get(currentLink);
-                //await save(await extract(driver));
+                await page.goto(currentLink);
+                let data = await extract(page);
+                await save(data);
             } catch (err){
-               
+               console.log(err)
             }
-        } while (crawl(requiredDepth--,driver ,(newlink) => currentLink = newlink));
+        } while (crawl(requiredDepth--,page ,(newlink) => currentLink = newlink));
     }
 
     //TODO: send an event to notify that the crawling has ended and the data is ready.
