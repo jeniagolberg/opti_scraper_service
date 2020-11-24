04369d58e5a5e27e47d7d7d074931295cfb68947
Jenia Golberg <jeniagolberg@Jenias-MacBook-Air.local>
2020-11-24 16:08:19 +0200

    added end point to read data

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ const PORT = process.env.PORT || "3001";
 
 app.get('/scrape', scrapeController);
 
+app.get('/getMovieData', (req,res,next) => {
+    try{
+        res.status(200);
+        res.send(require('./../movie-data.json'));
+    } catch( err ){
+        console.err(err)
+        res.status(400)
+        res.send('Error on fetching movie data from json')
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`server listening on port: ${PORT}`);
